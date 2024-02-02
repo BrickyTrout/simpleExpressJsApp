906dfa6fdc2343b60ba8c482151c44c240ccd98a
Arthur Wong <100111350+BrickyTrout@users.noreply.github.com>
2024-02-02 16:11:11 +1100

    write to json, post method

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,35 @@
 const express = require("express");
+const fs = require("fs").promises;
 
 const router = express.Router();
-const users = [{ name: "arthur" }, { name: "aimee" }, { name: "dani" }];
 
-router.get("/", (req, res) => {
-  console.log(req.query.name);
+router.get("/", async (req, res) => {
+  const users = await getUsersFromDb();
   res.render("users/list", { users });
 });
 router.get("/new", (req, res) => {
   res.render("users/new", { firstName: "enter your name" });
 });
-router.post("/", (req, res) => {
-  const isValid = false;
-  if (isValid) {
-    users.push({ name: req.body.firstName });
-    res.redirect(`/users/${users.length - 1}`);
-  } else {
-    console.log("error submitting new user");
-    res.render("users/new", { firstName: req.body.firstName });
-  }
+router.post("/", async (req, res) => {
+  const name = req.body.firstName;
+  const isValid = name.length > 0;
+  // if (isValid) {
+  const users = await getUsersFromDb();
+  console.log("users", users);
+  users.push({ name });
+  const json = JSON.stringify(users);
+  await fs.writeFile("users.json", json, "utf8");
+  res.redirect(`/users/${users.length - 1}`);
+  // } else {
+  //   console.log("error submitting new user");
+  //   res.render("users/new", { firstName: name });
+  // }
 });
 
 // middleware to retrieve user info given user id
-router.param("id", (req, res, next, id) => {
+router.param("id", async (req, res, next, id) => {
   console.log(id);
+  const users = await getUsersFromDb();
   req.user = users[id];
   next();
 });
@@ -44,5 +50,13 @@ router
     const id = req.params.id;
     res.send(`Delete user with  ID ${id}`);
   });
+
+async function getUsersFromDb() {
+  const data = await fs.readFile("users.json", "utf8");
+  console.log(data);
+  const parsed = JSON.parse(data);
+  console.log("parsed", parsed);
+  return parsed;
+}
 
 module.exports = router;
